fed7c5138ea14ada21ed9f4228aef47c8f88f47d
Uniqus <uniqus@gmail.com>
2013-07-27 03:17:07 +0300

    Simplified code and added some comments

--- src/main/scala/code/snippet/MySampleForm.scala
+++ src/main/scala/code/snippet/MySampleForm.scala
@@ -21,7 +21,6 @@ final case class SampleItem(id: Long, name: String)
 class MySampleForm extends Loggable {
   lazy val sampleItems = List(SampleItem(1, "Item #1"), SampleItem(2, "Item #2"), SampleItem(3, "Item #3"), SampleItem(4, "And one more to be sure"), SampleItem(5, "Well, the last one"), SampleItem(6, "Stop me please!"))
   lazy val selectedItemIds = ArrayBuffer[Long]()
-  protected var dataTableId = ""
   //  protected var dataTableColumnDef: Seq[String] = Seq()
 
   def renderItem(item: SampleItem): NodeSeq => NodeSeq = {
@@ -36,33 +35,50 @@ class MySampleForm extends Loggable {
   }
 
   def renderDataTable(in: NodeSeq): NodeSeq = {
-    val rowTemplate = ((in \\ "tr").filter(_.attribute("data-row-template").isDefined) \ "td")
-    val renderRowFunction = (ignore: String) => {
+    // This template is used to render single item
+    val itemTemplate: NodeSeq = <td><input type="checkbox"/></td><td><span class="item-id"></span></td><td><span class="item-name"></span></td>;
+
+    val getJSONRows = (ignore: String) => {
+      // This function provides data to DataTables via AJAX request
       val json = ("iTotalRecords" -> sampleItems.length) ~
         ("iTotalDisplayRecords" -> sampleItems.length) ~
         ("sEcho" -> S.param("sEcho").dmap(0)(_.toInt)) ~
         ("aaData" -> sampleItems.map(
-          r => renderItem(r).apply(rowTemplate).map(
-            t => JString(t.child.map(a => Html5.toString(a)).reduce((a, b) => a + b)))))
+          /* Here we take a list of sampleItes, and rendering them to
+             * List[NodeSeq] using renderItem transform on itemTemplate for each item */
+          item => renderItem(item).apply(itemTemplate).map(
+            /* Here we just transform each rendered item NodeSeq to string to pass it as a JSON array of strings */
+              /* TD is stripped by using t.child instead of t */
+            renderedHtml => JString(renderedHtml.child.map(a => Html5.toString(a)).reduce((a, b) => a + b)))))
       JsonResponse(json)
+    /*
+     * The resulting JSON will look like:
+{
+  "iTotalRecords":6,
+  "iTotalDisplayRecords":6,
+  "sEcho":2,
+  "aaData":[
+	  ["<input type=\"checkbox\" name=\"F750933123242NG3E2O\" value=\"true\"><input type=\"hidden\" name=\"F750933123242NG3E2O\" value=\"false\">","1","Item #1"],
+	  ["<input type=\"checkbox\" name=\"F750933123243RCFCHR\" value=\"true\"><input type=\"hidden\" name=\"F750933123243RCFCHR\" value=\"false\">","2","Item #2"],
+	  ...
+  ]
+}
+     *   
+     * 
+     */
+      
     }
 
-    S.fmapFunc(S.SFuncHolder(renderRowFunction)) { func =>
+    S.fmapFunc(S.SFuncHolder(getJSONRows)) { func =>
       val where: String = S.encodeURL(S.contextPath + "/" + LiftRules.ajaxPath + "?" + func + "=foo")
 
-      //      val id = idOpt getOrElse Helpers.nextFuncName
-      dataTableId = S.attr("data-table-id") getOrElse Helpers.nextFuncName
-      val jsonConfigFn = S.attr("data-table-config-fn")
-      val defaultConfig = """{ bProcessing: true, bServerSide: true, sAjaxSource: """ + where.encJs + "}"
-      val config = jsonConfigFn match {
-        case Full(s) => """jQuery.extend({}, """ + s + """(), """ + defaultConfig + """)"""
-        case _ => defaultConfig
-      }
-      val datatableOptions = JE.JsRaw(config)
+      // Here we are passing mapped getJSONRows function name to DataTables to be able to request data via AJAX
+      val dataTableConfig = """{ bProcessing: true, bServerSide: true, sAjaxSource: """ + where.encJs + "}"
 
+      // Here we are initializing jquery DataTable using defaultConfig
       val onLoad = JE.JsRaw("""
         jQuery(document).ready(function() {
-			$("#""" + dataTableId + """").dataTable(""" + datatableOptions.toJsCmd + """);
+			$("#data-tables-test").dataTable(""" + dataTableConfig + """);
 		});""").cmd
 
       in ++ <head_merge>
